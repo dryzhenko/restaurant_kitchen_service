60b3465d2cbcbacbe0cadc89b6403a75f7928df1
Daniil <daniilrizhenko@gmail.com>
2024-09-12 18:47:33 +0300

    create index func to represent home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from catalog.models import Dish, DishType, Cook
 
-# Create your views here.
+
+def index(request):
+    num_cookers = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    data = {
+        "num_cookers": num_cookers,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+    }
+
+    return render(request, "catalog/index.html", context=data)
