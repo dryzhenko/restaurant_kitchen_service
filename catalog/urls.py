10a5d720c6da139ef1dab2f67b3ae9ee58a17055
Daniil <daniilrizhenko@gmail.com>
2024-09-13 13:01:23 +0300

    create paths for views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import index, CookListView, CookDetailView, DishListView, DishDetailView, DishTypeListView, DishTypeDetailView, IngredientListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("cook/", CookListView.as_view(), name="cook-list"),
+    path("cook/<int:pk>", CookDetailView.as_view(), name="cook-detail"),
+    path("dish/", DishListView.as_view(), name="dish-list"),
+    path("dish/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
+    path("dish/type/", DishTypeListView.as_view(), name="dish_type-list"),
+    path("dish/type/<int:pk>", DishTypeDetailView.as_view(), name="dish_type-detail"),
+    path("ingredient/", IngredientListView.as_view(), name="ingredient-list"),
 ]
 
 app_name = "catalog"
