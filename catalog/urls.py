9abf64a8692258d5b1669f10ced763dd921585f3
Daniil <daniilrizhenko@gmail.com>
2024-09-18 17:49:31 +0300

    add pathes for new views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 
-from catalog.views import index, CookListView, CookDetailView, DishListView, DishDetailView, DishTypeListView, DishTypeDetailView
+from catalog.views import (
+    index,
+    CookListView,
+    CookDetailView,
+    DishListView,
+    DishDetailView,
+    DishTypeListView,
+    DishTypeDetailView,
+    DishCreateView,
+    DishUpdateView,
+    DishDeleteView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
@@ -8,8 +22,14 @@ urlpatterns = [
     path("cook/<int:pk>", CookDetailView.as_view(), name="cook-detail"),
     path("dish/", DishListView.as_view(), name="dish-list"),
     path("dish/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
+    path("dish/create/", DishCreateView.as_view(), name="dish-create"),
+    path("dish/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
+    path("dish/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
     path("dish/type/", DishTypeListView.as_view(), name="dish_type-list"),
     path("dish/type/<int:pk>", DishTypeDetailView.as_view(), name="dish_type-detail"),
+    path("dish/type/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
+    path("dish/type/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path("dish/type/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
 ]
 
 app_name = "catalog"
